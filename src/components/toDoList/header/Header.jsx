33a662ed8ec81224e1c486c85999f69e26d78ece
MyungWanPark <mywanpark@gmail.com>
2022-11-27 14:27:16 +0900

    Feat: Structure project

---
+++ src/components/toDoList/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LightDarkBtn from './LightDarkBtn';
+import Tab from './Tab';
+
+export const Header = (props) => {
+    return (
+        <div>
+            <LightDarkBtn />
+            <Tab />
+        </div>
+    )
+};
---
+++ src/components/toDoList/header/LightDarkBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BsFillSunFill } from 'react-icons/bs'
+
+export default function LightDarkBtn() {
+    return (
+        <div>
+            <button><BsFillSunFill /></button>
+        </div>
+    );
+}
+
