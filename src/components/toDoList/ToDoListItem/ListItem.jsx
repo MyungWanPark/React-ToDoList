8e67c1d0169619e23bedb4f855d4b386594c71bf
MyungWanPark <mywanpark@gmail.com>
2022-12-11 22:40:49 +0900

    Style: Change check box accent color

--- src/components/toDoList/ToDoListItem/ListItem.jsx
+++ src/components/toDoList/ToDoListItem/ListItem.jsx
@@ -3,6 +3,7 @@ import { BsTrash } from "react-icons/bs";
 import styles from "./ListItem.module.css";
 
 export default function ListItem({ toDo, onUpdate, onDelete }) {
+    const { id, status, text } = toDo;
     const handleChange = (e) => {
         const status = e.target.checked ? "completed" : "active";
         onUpdate({
@@ -19,15 +20,13 @@ export default function ListItem({ toDo, onUpdate, onDelete }) {
         <li className={styles.listItem}>
             <input
                 type="checkbox"
-                id={`${toDo.id}`}
-                checked={toDo.status === "completed"}
+                id={id}
+                checked={status === "completed"}
                 onChange={handleChange}
                 className={styles.input}
             />
-            <label
-                htmlFor={`${toDo.id}`}
-                className={styles.text}>
-                {toDo.text}
+            <label htmlFor={id} className={styles.text}>
+                {text}
             </label>
             <button
                 className={styles.button}
